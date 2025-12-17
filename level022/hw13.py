0a313184bed15eb968553a95ef08fa656acba391
mariphilishvili <mariphilishvili@gmail.com>
2025-12-17 21:17:09 +0400

    commit

---
+++ level022/hw13.py
@@ -0,0 +1,15 @@
+# )შექმენი ცვლადი სადაც შეინახავ რიცხვებით სავსე სიას
+
+# შენი დავალებაა შეამოწმო -->
+
+# თუ სიაში მყოფი მეორე ინდექსზე მდგომი ელემენტი არის ლუწი
+#     დაპრნტე --> the element on second index is even
+# სხვა შემთხვევაში ---->
+#     დაპრინტე --> the element on second index is odd
+
+numbers = [5, 12, 7, 9, 20]
+if numbers[2] % 2 == 0:
+    print("the element on second index is even")
+else:
+    print("the element on second index is odd")
+
